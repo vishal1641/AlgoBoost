7fa131b8175db4e5d01b1b498d9c0aff0c63fba1
vishal1641 <tiwarikumarjio.vishal123@gmail.com>
2024-12-31 14:32:39 +0530

    deshaw

--- src/components/header/RollingGallery.jsx
+++ src/components/header/RollingGallery.jsx
@@ -17,7 +17,7 @@ const RollingGallery = ({ images = [] }) => {
               transform: `rotateY(${index * angleStep}deg) translateZ(${radius}px)`,
             }}
           >
-           <img src={url} alt=""/>
+           <img src={url} alt="DE SHAW"/>
 
           </div>
         ))}
