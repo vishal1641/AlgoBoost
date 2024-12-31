1033bab0c06db8dec8b6038496881257c51030b1
vishal1641 <tiwarikumarjio.vishal123@gmail.com>
2024-12-31 14:28:44 +0530

    authentication removed

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,16 +1,16 @@
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+// import { initializeApp } from "firebase/app";
+// import { getAuth } from "firebase/auth";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyCHnoTFniHmSsvZZOmPefssx3nbeN0CWzk",
-  authDomain: "vktproject-91360.firebaseapp.com",
-  projectId: "vktproject-91360",
-  storageBucket: "vktproject-91360.firebasestorage.app",
-  messagingSenderId: "72103638244",
-  appId: "1:72103638244:web:7e366ebb70a463bacba8f1",
-};
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCHnoTFniHmSsvZZOmPefssx3nbeN0CWzk",
+//   authDomain: "vktproject-91360.firebaseapp.com",
+//   projectId: "vktproject-91360",
+//   storageBucket: "vktproject-91360.firebasestorage.app",
+//   messagingSenderId: "72103638244",
+//   appId: "1:72103638244:web:7e366ebb70a463bacba8f1",
+// };
 
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
+// const app = initializeApp(firebaseConfig);
+// const auth = getAuth(app);
 
-export { app, auth };
+// export { app, auth };
